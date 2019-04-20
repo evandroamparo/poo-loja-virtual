fbd2b94e9f5fbe09c7d6f79970a64d5bd61a5e5a
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2019-04-20 00:47:30 -0300

    commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace poo_loja_virtual.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
